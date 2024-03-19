b1fb142533994839576bd862edafbae804caa2a9
Ronnie Maingi <161239857+ronniemaingi@users.noreply.github.com>
2024-03-19 10:40:19 +0300

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+def generate_fibonacci(n):
+    fibonacci_sequence = [0, 1]
+    for i in range(2, n):
+        next_term = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_term)
+    return fibonacci_sequence
+
+def main():
+    n = int(input("Enter the value of n: "))
+    if n <= 0:
+        print("Please enter a positive integer for n.")
+    else:
+        fibonacci_sequence = generate_fibonacci(n)
+        print("Fibonacci sequence up to term", n, ":", fibonacci_sequence)
+
+if __name__ == "__main__":
+    main()
